93be6fc26494a081afe0a89d48be72f423e1af0d
HanLin <3358454532@qq.com>
2026-05-01 21:52:20 +0800

    类，类的定义，属性，方法，构造函数，变量声明赋值，

--- StudySystem/SystemClient/Program.cs
+++ StudySystem/SystemClient/Program.cs
@@ -1,4 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Net.Http.Headers;
+using Util.Model;
+
 Console.WriteLine("Hello, World!");
+
+int number = 12;
+long largeNumber = 13;
+float abc = 12;
+double abcd = 0.34;
+string abcddd = "this is dfsdfoex";
+bool tt = true;
+char c = '1';
+int a = 1;
+
+String aaa = new String("sssssss");
+Book book = new Book();
+Book book1 = new Book("AAA");
+Book bookA = new Book("Study", true, 50);
+
+string result = bookA.BookDetail();
 
 Console.ReadLine();
