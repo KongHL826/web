9ab28e00330e4682044343c971dc1dd166e9034d
HanLin <3358454532@qq.com>
2026-05-03 19:02:14 +0800

    学生基本信息 平均成绩

--- StudySystem/SystemClient/Program.cs
+++ StudySystem/SystemClient/Program.cs
@@ -4,16 +4,41 @@ using Util.Model;
 
 Console.WriteLine("Hello, World!");
 
+Console.WriteLine("请输入学生的学号");
+string number = Console.ReadLine();
+Console.WriteLine("请输入学生的姓名");
+string name = Console.ReadLine();
+Console.WriteLine("请输入学生的年龄");
+string age = Console.ReadLine();
+Console.WriteLine("请输入学生的语文成绩");
+string chinesegrade = Console.ReadLine();
+Console.WriteLine("请输入学生的数学成绩");
+string mathgrade = Console.ReadLine();
+Console.WriteLine("请输入学生的英语成绩");
+string engliashgrade = Console.ReadLine();
+int sage = int.Parse(age);
+int schinesegrade = int.Parse(chinesegrade);
+int smathgrade = int.Parse(mathgrade);
+int senglishgrade = int.Parse(engliashgrade);
 
 
-int number = 12;
-long largeNumber = 13;
-float abc = 12;
-double abcd = 0.34;
-string abcddd = "this is dfsdfoex";
-bool tt = true;
-char c = '1';
-int a = 1;
+
+
+
+
+
+Student student1 = new Student(number,name,sage,schinesegrade,smathgrade,senglishgrade); 
+int grade = student1.Study();
+Console.WriteLine("学生的平均成绩是"+grade);
+bool pingjun = student1.Jige();
+ //bool Jige()
+
+//bool pingjunchengji = student1.Jige();
+Console.WriteLine("学生的成绩及格"+pingjun);
+Console.WriteLine("学生的成绩不及格"+!pingjun);
+
+
+
 
 String aaa = new String("sssssss");
 Book book = new Book();
@@ -33,3 +58,4 @@ Console.WriteLine(mathclass1.Add());
 Console.WriteLine(mathclass1.Minus());
 Console.WriteLine(mathclass1.Times());
 Console.WriteLine(mathclass1.Devide());
+
---
+++ StudySystem/Util/Model/Student.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Util.Model
+{
+    public class Student
+    {
+        private string nmuber;
+        public string Nmuber
+        {
+            get { return nmuber; }
+            set { nmuber = value; }
+        }
+        private string name;    
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        private int age;    
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+        private int chinesegrade;
+        public int Chinesegrade
+        {
+            get { return chinesegrade; }
+            set { chinesegrade = value; }
+        }
+        private int mathgrade;
+        public int Mathgrade
+        {
+            get { return mathgrade; }
+            set { mathgrade = value; }
+        }
+        private int englishgrade;
+        public int Englishgrade
+        {
+            get { return englishgrade; }
+            set { englishgrade = value; }
+        }
+        public Student(string nmuber, string name, int age, int chinesegrade, int mathgrade, int englishgrade)
+        {
+            this.nmuber = nmuber;
+            this.name = name;
+            this.age = age;
+            this.chinesegrade = chinesegrade;
+            this.mathgrade = mathgrade;
+            this.englishgrade = englishgrade;
+        }
+        public int Study()
+        {
+            return (chinesegrade + mathgrade + englishgrade) / 3; 
+        }
+        public bool Jige()
+        {
+            return  Study() >= 60;
+
+        }
+    }
+
+}
