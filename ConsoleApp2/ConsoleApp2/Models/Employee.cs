839665ba542e69dc7b7cc099ef360b63bb8e11f6
HanLin <3358454532@qq.com>
2026-06-15 16:56:49 +0800

    第一天任务

---
+++ ConsoleApp2/ConsoleApp2/Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeSys.Models
+{
+    public class Employee
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Department { get; set; }
+        public decimal Salary { get; set; }
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/Services/EmployeeService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using EmployeeSys.Models;
+namespace EmployeeSys.Services
+{
+    public class EmployeeService
+    {
+        private List<Employee> _employees = new();
+        public void AddEmployee(Employee emp) => _employees.Add(emp);
+        public List<Employee> GetAll() => _employees;
+        public bool DeleteEmployee(string id)
+        {
+            var emp = _employees.FirstOrDefault(e => e.Id == id);
+            return emp != null && _employees.Remove(emp);
+        }
+    }
+}
